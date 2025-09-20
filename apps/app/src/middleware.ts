88a7132a7bb869b9b448203edfb3dba609ec16fc
Diego Sanchez <diegorsm03@gmail.com>
2025-09-20 12:07:33 -0300

    feat(app): last fix for middleware

--- apps/app/src/middleware.ts
+++ apps/app/src/middleware.ts
@@ -1,8 +1,26 @@
 import { NextResponse, NextRequest } from "next/server"
 import { Locale } from "./dictionaries/dictionaries"
+import { match } from "@formatjs/intl-localematcher"
+import Negotiator from "negotiator"
+
+const headers = { "accept-language": "en,es;q=0.5" }
+const languages = new Negotiator({ headers }).languages()
 
 const LOCALES: Locale[] = ["en", "fr"]
 const DEFAULT_LOCALE = "en"
+
+match(languages, LOCALES, DEFAULT_LOCALE)
+
+const getLocale = (request: NextRequest) => {
+  const { pathname } = request.nextUrl
+  const pathnameHasLocale = LOCALES.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  )
+
+  if (pathnameHasLocale) return pathname
+
+  return DEFAULT_LOCALE
+}
 
 export const middleware = (request: NextRequest) => {
   const { pathname } = request.nextUrl
@@ -10,10 +28,14 @@ export const middleware = (request: NextRequest) => {
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   )
 
-  if (!pathnameHasLocale)
-    return NextResponse.redirect(new URL(`/${DEFAULT_LOCALE}/404`, request.url))
+  if (pathnameHasLocale) return
 
-  return NextResponse.next()
+  if (pathname === "/") {
+    const locale = getLocale(request)
+    return NextResponse.redirect(new URL(`/${locale}`, request.url))
+  }
+
+  return NextResponse.redirect(new URL(`/${DEFAULT_LOCALE}/404`, request.url))
 }
 
 export const config = {
