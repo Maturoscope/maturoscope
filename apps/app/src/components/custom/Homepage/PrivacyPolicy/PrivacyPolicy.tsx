1339db12e6e5b986933180642f4a50b0a4954406
Diego Sanchez <diegorsm03@gmail.com>
2025-11-25 23:32:00 -0300

    feat: rebranding of footer

--- apps/app/src/components/custom/Homepage/PrivacyPolicy/PrivacyPolicy.tsx
+++ apps/app/src/components/custom/Homepage/PrivacyPolicy/PrivacyPolicy.tsx
@@ -8,24 +8,42 @@ import { SIMPLE_FADE_VARIANT } from "@/animations/common"
 
 export interface PrivacyPolicyProps {
   description: string
+  copyright: string
 }
 
-const PrivacyPolicy = ({ description }: PrivacyPolicyProps) => {
+const SYNOPP_URL = "https://synopp.io/"
+
+const PrivacyPolicy = ({ description, copyright }: PrivacyPolicyProps) => {
   return (
     <motion.div
       variants={SIMPLE_FADE_VARIANT}
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
-      className="flex gap-4 w-full max-w-[1280px] px-6 mt-11 lg:flex-row flex-col"
+      className="flex gap-4 w-full p-6 bg-white border border-border justify-between items-end"
     >
-      <Image
-        src="/icons/homepage/flag-europe.svg"
-        alt="Europe Flag"
-        width={90}
-        height={60}
-      />
-      <p className="text-sm text-muted-foreground">{description}</p>
+      <div className="flex lg:flex-row flex-col items-center gap-4">
+        <Image
+          src="/icons/homepage/flag-europe.svg"
+          alt="Europe Flag"
+          width={72}
+          height={48}
+        />
+        <p className="text-xs text-muted-foreground max-w-[765px]">
+          {description}
+        </p>
+      </div>
+      <p className="text-sm text-muted-foreground">
+        {copyright}{" "}
+        <a
+          href={SYNOPP_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline font-medium"
+        >
+          Synopp
+        </a>
+      </p>
     </motion.div>
   )
 }
