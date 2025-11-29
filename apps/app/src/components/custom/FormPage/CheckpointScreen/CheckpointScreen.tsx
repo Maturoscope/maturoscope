5ae5a41ab62fb2100b4bd111bb8290bec69dba8d
Diego Sanchez <diegorsm03@gmail.com>
2025-11-29 15:33:28 -0300

    feat: handle theme color change for checkpoint screen

--- apps/app/src/components/custom/FormPage/CheckpointScreen/CheckpointScreen.tsx
+++ apps/app/src/components/custom/FormPage/CheckpointScreen/CheckpointScreen.tsx
@@ -1,8 +1,8 @@
-// Packages
-import Image from "next/image"
 // Components
 import Heading from "@/components/common/Heading/Heading"
 import { Button } from "@/components/ui/button"
+import { ArrowNextIcon } from "@/components/icons"
+import { getIconComponent } from "@/components/icons/iconMap"
 
 interface CheckpointScreenProps {
   icon: string
@@ -23,9 +23,11 @@ const CheckpointScreen = ({
   onButtonClick,
   onReviewClick,
 }: CheckpointScreenProps) => {
+  const IconComponent = getIconComponent(icon)
+
   return (
     <div className="flex flex-col items-center justify-center w-full h-full max-w-[1280px] px-6 pb-26 pt-24 lg:pt-0">
-      <Image src={icon} alt={title} width={60} height={60} />
+      {IconComponent && <IconComponent accent className="w-[60px] h-[60px]" />}
       <Heading
         title={title}
         description={description}
@@ -38,14 +40,9 @@ const CheckpointScreen = ({
         >
           <span className="hidden lg:block">{reviewLabel}</span>
         </Button>
-        <Button onClick={onButtonClick}>
+        <Button onClick={onButtonClick} accent>
           <span className="hidden lg:block">{buttonLabel}</span>
-          <Image
-            src="/icons/form/arrow-next.svg"
-            alt="Arrow Next"
-            width={16}
-            height={16}
-          />
+          <ArrowNextIcon className="w-4 h-4" />
         </Button>
       </div>
     </div>
