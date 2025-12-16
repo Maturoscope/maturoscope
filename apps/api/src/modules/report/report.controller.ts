0b9db6ddb59f675821271c2ea24844151e3ec3ea
Diego Sanchez <diegorsm03@gmail.com>
2025-12-16 19:17:49 -0300

    refactor(pdf): implement a more robust option for PDF styles

--- apps/api/src/modules/report/report.controller.ts
+++ apps/api/src/modules/report/report.controller.ts
@@ -1,21 +1,22 @@
 // Packages
-import { Controller, Get, Param } from '@nestjs/common';
-import { StreamableFile } from '@nestjs/common';
+import { Controller, Get, Header, Param, StreamableFile } from '@nestjs/common';
 // Services
 import { ReportService } from './report.service';
-
-const STREAMABLE_FILE_OPTIONS = {
-  disposition: 'inline; filename=report.pdf',
-  type: 'application/pdf; charset=utf-8',
-};
 
 @Controller('report')
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
 
   @Get(':id')
-  async getPDF(@Param('id') id: string) {
+  @Header('Content-Type', 'application/pdf')
+  @Header('Content-Disposition', 'inline; filename=report.pdf')
+  @Header('Cache-Control', 'no-cache, no-store, must-revalidate')
+  async getPDF(@Param('id') id: string): Promise<StreamableFile> {
     const buffer = await this.reportService.getPDF(id);
-    return new StreamableFile(buffer, STREAMABLE_FILE_OPTIONS);
+
+    return new StreamableFile(buffer, {
+      type: 'application/pdf',
+      length: buffer.length,
+    });
   }
 }
